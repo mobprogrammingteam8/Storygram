a793d697b9882e2dbbaa77defe4e4d3157e1b92b
chaehyeon <113291325+chaehyeon107@users.noreply.github.com>
2023-12-13 19:17:51 +0900

    Story
    git commit -m "GLPM-15 <message>"

--- app/src/main/java/com/example/home33/Transformers.kt
+++ app/src/main/java/com/example/home33/Transformers.kt
@@ -1,61 +1,83 @@
-package com.example.storyactivity
+package com.example.diaryproject
 
 import android.graphics.Color
-import android.graphics.Typeface
 import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.text.Spanned
+import android.text.style.BackgroundColorSpan
 import android.text.style.ForegroundColorSpan
-import android.text.style.RelativeSizeSpan
 import android.text.style.StrikethroughSpan
-import android.text.style.StyleSpan
-import android.text.style.SuperscriptSpan
 import com.greenfrvr.hashtagview.HashtagView
 import com.greenfrvr.hashtagview.HashtagView.DataStateTransform
 import com.greenfrvr.hashtagview.HashtagView.DataTransform
-import java.util.Locale
 
-
-class Transformers {
-    val HASH_SELECTED: DataTransform<String> = object : DataStateTransform<String> {
+class Transformers : DataStateTransform<String> {
+    val HASH: DataTransform<String> = object : DataStateTransform<String> {
         override fun prepare(item: String): CharSequence {
             val spannableString = SpannableString("#$item")
             spannableString.setSpan(
-                ForegroundColorSpan(Color.parseColor("#85F5F5F5")),
+                ForegroundColorSpan(Color.parseColor("#black")),
                 0,
-                1,
+                spannableString.length,
                 Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
             )
+            return spannableString
+        }
+        // HASH : object 때문에 선언해야함
+        override fun prepareSelected(item: String?): CharSequence {
+            val spannableString = SpannableStringBuilder("#$item")
+            spannableString.insert(0, item)
+            spannableString.setSpan(
+                ForegroundColorSpan(Color.parseColor("red")),
+                0,
+                spannableString.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
             return spannableString
         }
 
-        override fun prepareSelected(item: String): CharSequence {
-            val spannableString = SpannableString("#$item")
-            spannableString.setSpan(
-                ForegroundColorSpan(Color.parseColor("#85F5F5F5")),
-                0,
-                spannableString.length,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            spannableString.setSpan(
-                StrikethroughSpan(),
-                0,
-                spannableString.length,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            return spannableString
-        }
+
+    }
+    //DataStateTransform에서 상속받으므로 선언해주어야함
+    override fun prepare(item: String?): CharSequence {
+        val spannableString = SpannableStringBuilder("#$item")
+        spannableString.insert(0, item)
+        spannableString.setSpan(
+            ForegroundColorSpan(Color.parseColor("red")),
+            0,
+            spannableString.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        return spannableString
     }
 
-    companion object {
-        val HASH = DataTransform<String> { item ->
-            val spannableString = SpannableString("#$item")
+    override fun prepareSelected(item: String?): CharSequence {
+        val spannableString = SpannableStringBuilder("#$item")
+        spannableString.insert(0, item)
+        spannableString.setSpan(
+            ForegroundColorSpan(Color.parseColor("red")),
+            0,
+            spannableString.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        return spannableString
+    }
+    // 실제 코드 사용부분 HASH_SELECTED
+    companion object HASH_SELECTED: HashtagView.DataTransform<String> {
+        override fun prepare(item: String?): CharSequence {
+            val spannableString = SpannableStringBuilder("#$item")
+            spannableString.insert(0, item)
             spannableString.setSpan(
-                ForegroundColorSpan(Color.parseColor("#85F5F5F5")),
+                ForegroundColorSpan(Color.parseColor("red")),
                 0,
-                1,
+                spannableString.length,
                 Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
             )
-            spannableString
+
+            return spannableString
         }
     }
 }
